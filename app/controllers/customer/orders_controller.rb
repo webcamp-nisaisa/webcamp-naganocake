8556922daf701db32237bbc178026b18699fbb95
takahashi-1234 <riki.1130@icloud.com>
2021-11-18 01:00:40 +0000

    add customer_shippings

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -1,2 +1,25 @@
 class Customer::OrdersController < ApplicationController
+  def new
+    @order=Order.new
+    @credit_card=Order.payment_methods.key(0)
+    @transfer=Order.payment_methods.key(1)
+    @credit_card_ja=Order.payment_methods_i18n[:credit_card]
+    @transfer_ja=Order.payment_methods_i18n[:transfer]
+  end
+
+  def confirm
+    params[:order][:select_address]
+  end
+
+  def create
+  end
+
+  def complete
+  end
+  
+  private
+  def order_params
+    params.require(:order).permit(:payment_method)
+  end
+
 end
--- app/controllers/customer/shippings_controller.rb
+++ app/controllers/customer/shippings_controller.rb
@@ -1,21 +1,30 @@
 class Customer::ShippingsController < ApplicationController
   def index
-    @shippings=Shipping.all
     @shipping=Shipping.new
+    @shippings=current_customer.shippings
   end
 
   def create
-    current_user.shippings.create(shipping_params)
+    shipping=Shipping.new(shipping_params)
+    shipping.customer_id=current_customer.id
+    shipping.save
     redirect_back(fallback_location:root_path)
   end
 
   def destroy
+    shipping=Shipping.find(params[:id])
+    shipping.destroy
+    redirect_back(fallback_location:root_path)
   end
 
   def edit
+    @shipping=Shipping.find(params[:id])
   end
 
   def update
+    @shipping=Shipping.find(params[:id])
+    @shipping.update(shipping_params)
+    redirect_to shippings_path
   end
 
   private
