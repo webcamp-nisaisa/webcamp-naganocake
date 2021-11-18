7eea7ddf9fdc447192482ca781c9839e7375911f
takahashi-1234 <riki.1130@icloud.com>
2021-11-18 06:38:15 +0000

    [add]seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = Admin.new(:email => 'admin@admin.com', :password => 'naganocake')
+admin.save!
