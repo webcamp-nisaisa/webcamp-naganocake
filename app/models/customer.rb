09269a2811aaf501e2d4782c32723cc7e0b94695
KamiyaKeisuk <biotite078260@gmail.com>
2021-11-18 15:41:06 +0000

    [Add] admin_customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,4 +7,12 @@ class Customer < ApplicationRecord
   has_many:orders,dependent: :destroy
   has_many:cart_items,dependent: :destroy
   has_many:shippings,dependent: :destroy
+  
+  def full_name
+    self.last_name + self.first_name
+  end
+  
+  def full_name_kana
+    self.last_name_kana + self.first_name_kana
+  end
 end
